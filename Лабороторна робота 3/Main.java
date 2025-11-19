195d269b519589bcb01af404ddeee580da17ec50
anastassia <nastaglinka2@gmail.com>
2025-11-19 17:51:52 +0200

    Revise cart items and order bill output
    Updated cart items and modified order processing.

--- Лабороторна робота 3/Main.java
+++ Лабороторна робота 3/Main.java
@@ -1,35 +1,29 @@
+package org.example.task2;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("=== BOX ===");
-        Box b = new Box(2, 3, 4);
-        System.out.println("Surface area: " + b.surfaceArea());
-        System.out.println("Lateral area: " + b.lateralSurfaceArea());
-        System.out.println("Volume: " + b.volume());
-
-        System.out.println("\n=== CART ===");
         Cart cart = new Cart(10);
 
-        cart.add(new Item(1, "Laptop", 25000));
-        cart.add(new Item(2, "Phone", 18000));
-        cart.add(new Item(3, "TV", 30000));
+        cart.add(new Item(1, "Samsung Galaxy S23", 27999));
+        cart.add(new Item(2, "Lenovo IdeaPad 3", 19499));
+        cart.add(new Item(3, "LG 55\" 4K Smart TV", 15999));
+        cart.add(new Item(4, "Bosch Serie 6 Пральна машина", 14799));
+        cart.add(new Item(5, "Samsung RB34 Холодильник", 22399));
+        cart.add(new Item(6, "De'Longhi Magnifica S Кавоварка", 10599));
+        cart.add(new Item(7, "Gorenje Електрична плита", 9999));
+        cart.add(new Item(8, "Dyson V11 Пилосос", 16499));
+        cart.add(new Item(9, "Samsung ME83K Мікрохвильова піч", 3199));
+        cart.add(new Item(10, "Philips DryCare Фен", 1499));
 
         System.out.println(cart);
 
-        cart.removeById(2);
+        cart.removeById(9);
+
         System.out.println(cart);
 
-        Order order = new Order(1, "John");
-        System.out.println(order.formOrderBill(cart));
-
-        System.out.println("\n=== STACK ===");
-        IntStack st = new IntStack();
-        st.push(10);
-        st.push(20);
-        st.push(30);
-
-        System.out.println("Top: " + st.peek());
-        System.out.println("Pop: " + st.pop());
-        System.out.println("Size: " + st.size());
+        Order order = new Order(1L, "John");
+        String bill = order.formOrderBill(cart);
+        System.out.println(bill);
     }
 }
