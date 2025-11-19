77c7a992100f9306d94dc7ec5c26b1a7f415a386
anastassia <nastaglinka2@gmail.com>
2025-11-19 17:46:14 +0200

    Update and rename IntStack.java to Item.java

---
+++ Лабороторна робота 3/Item.java
@@ -0,0 +1,21 @@
+public class Item {
+    private long id;
+    private String name;
+    private double price;
+
+    public Item(long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return "Item{id=" + id + ", name='" + name + "', price=" + price + "}\n";
+    }
+}
+
