4f326848f5add09b5adb824d19b96cbd77b96e28
anastassia <nastaglinka2@gmail.com>
2025-11-24 21:27:40 +0200

    Refactor Order class constructor and methods
    Refactored Order class to use customer name and Cart object. Removed old constructor and billing method.

--- Лабороторна робота 3/Order.java
+++ Лабороторна робота 3/Order.java
@@ -1,38 +1,19 @@
-package org.example.task2;
-
 public class Order {
+    private static int idCounter = 1; // Лічильник для ID замовлень
+    private int orderId;
+    private Cart cart;
+    private String customerName;
 
-    private long id;
-    private String customer;
-
-    public Order(long id, String customer) {
-        this.id = id;
-        this.customer = customer;
+    public Order(String customerName, Cart cart) {
+        this.orderId = idCounter++;
+        this.customerName = customerName;
+        this.cart = cart;
     }
 
-    public String formOrderBill(Cart cart) {
-
-        StringBuilder builder = new StringBuilder();
-        builder.append("Order number ").append(id)
-                .append(" for customer ").append(customer)
-                .append("\n------------------\n");
-
-        double sum = 0.0;
-
-        for (int i = 0; i < cart.getSize(); i++) {
-            Item item = cart.getItem(i);
-            sum += item.getPrice();
-
-            builder.append("Item id: ").append(item.getId())
-                    .append(" name: ").append(item.getName())
-                    .append(" price: ").append(item.getPrice())
-                    .append("\n");
-        }
-
-        builder.append("------------------\nTotal sum: ")
-                .append(sum);
-
-        return builder.toString();
+    public void showOrder() {
+        System.out.println("\n=== ЗАМОВЛЕННЯ №" + orderId + " ===");
+        System.out.println("Клієнт: " + customerName);
+        cart.showCart();
+        System.out.println("======================\n");
     }
 }
-
